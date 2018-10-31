99b4aa3b40532209e9f20cfe68d73b3457e62ef7
shawndale Shogbamimu <P2510314@LEC-ADMIN.dmu.ac.uk>
2018-10-31 11:01:23 +0000

    changes to original

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("hello");
+
+const menuToggle = getElementById('menu-toggle');
+const menuNav = getElementById('menu-nav');
+
+(toggleMenu)=>{
+console.log("called toggleMenu");
+
+}
+
+menuToggle.addEventListner("click", toggleMenu);
